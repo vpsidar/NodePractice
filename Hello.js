6fe60594fd42e65b5531d36f6838ec3243fc1474
vpsidar <vpmsidar@gmail.com>
2021-05-19 10:41:38 +0530

    Demo Requested Url Access

--- Hello.js
+++ Hello.js
@@ -3,5 +3,6 @@ var dt = require('./modules/myfirstmodule');
 // Initial Content Adding more Content
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Current Date And Time is :' + dt.myDateTime());
+    res.write('Current Date And Time is :' + dt.myDateTime());
+    res.end();
 }).listen(8080);
