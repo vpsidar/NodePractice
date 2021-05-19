f93b98d3773f319a2c1dce43d9c21534bc6f0729
vpsidar <vpmsidar@gmail.com>
2021-05-19 10:33:16 +0530

    External Modules

--- Hello.js
+++ Hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var dt = require('./modules/myfirstmodule');
 // Initial Content Adding more Content
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World! From Vijay');
+    res.end('Current Date And Time is :' + dt.myDateTime());
 }).listen(8080);
